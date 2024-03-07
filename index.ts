6625a3bec8d8f7d9d49421386995930fe1b5d553
wetzernutz <126750514+wetzernutz@users.noreply.github.com>
2024-03-07 21:11:34 +1100

    extracted method from draw
    extracted and created a new createGraphics function.

--- index.ts
+++ index.ts
@@ -113,13 +113,17 @@ function update() {
 }
 
 function draw() {
-  let canvas = document.getElementById("GameCanvas") as HTMLCanvasElement;
-  let g = canvas.getContext("2d");
-
-  g.clearRect(0, 0, canvas.width, canvas.height);
-
+  let g = createGraphics();
   drawMap(g);
   drawPlayer(g);
+}
+
+function createGraphics() {
+  let canvas = document.getElementById("GameCanvas") as HTMLCanvasElement;
+  let g = canvas.getContext("2d");
+  g.clearRect(0, 0, canvas.width, canvas.height);
+
+  return g;
 }
 
 function drawMap(g: CanvasRenderingContext2D){
